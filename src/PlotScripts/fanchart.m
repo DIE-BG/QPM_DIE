36adc40c9a209f0ac1a82a60f8d510109d293f69
Marianojgm86 <59839138+Marianojgm86@users.noreply.github.com>
2024-08-13 15:02:06 -0600

    Revert "Ajustes Fanchart"
    This reverts commit a8b61c7d9982affa94e0994a2cecaba6be2e4f21.

--- src/PlotScripts/fanchart.m
+++ src/PlotScripts/fanchart.m
@@ -273,9 +273,8 @@ codigo_color=abs(codigo_color);
         axis on
         % Tabla
         % Fechas
-        if isempty(params.TabRange)
-            params.TabRange = MODEL.DATES.Fanchart;
-        end
+        params.TabRange = MODEL.DATES.Fanchart;
+        
         table_p = uipanel( ...
             main_p, ...
             'Position', [0, 1 - 0.9 - 0.10, 1, 0.10], ...
