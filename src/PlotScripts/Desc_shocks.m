c9bc9d31cef796f3ad55de87ef76d3ca41539395
Jorge Orenos <orenosjorge@gmail.com>
2024-10-07 15:25:26 -0600

    Modificaciones al código de la plataforma de ejecucución
    Se modifica el código para que todas las concordancias con SimTools. sean cambiadas por Sim. y utilizar la carpeta que se agregó al path
    
    Co-Authored-By: r2cp <5157100+r2cp@users.noreply.github.com>

--- src/PlotScripts/Desc_shocks.m
+++ src/PlotScripts/Desc_shocks.m
@@ -4,18 +4,18 @@
     seleccionadas (list), en dos diferentes rangos. 
 %}
 % Generación de datos
-MODEL = SimTools.sim.shd_dsc(MODEL);
+MODEL = Sim.sim.shd_dsc(MODEL);
 % Lista de variables a graficar
 % sh_list = get(MODEL.MF, 'xlist');;
 
 %% Gráficas
 % Rango Completo
 temp_path = fullfile('plots',MODEL.CORR_DATE,MODEL.CORR_VER,'Shock_dec\long');
-SimTools.scripts.plot_shd_dsc(MODEL, 'SavePath', temp_path,...
+Sim.scripts.plot_shd_dsc(MODEL, 'SavePath', temp_path,...
                              'Rng', {},...
                              'Variables',sh_list);
 % Rango corto
 temp_path = fullfile('plots',MODEL.CORR_DATE,MODEL.CORR_VER,'Shock_dec\short');
-SimTools.scripts.plot_shd_dsc(MODEL, 'SavePath', temp_path,...
+Sim.scripts.plot_shd_dsc(MODEL, 'SavePath', temp_path,...
                              'Rng', MODEL.DATES.hist_end-20:MODEL.DATES.hist_end+20,...
                              'Variables',sh_list); 
